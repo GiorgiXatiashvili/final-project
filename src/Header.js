b89053b3fb6ee2ef95327277c3372d12cb01fd74
giorgi xatiashvili <giorgixati123@gmail.com>
2020-05-19 17:49:59 +0400

    add

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-function NavMenu() {
-    return(
-        <div>
-            <h1>jigari</h1>
-        </div>  
-    );
-};
+function Header() {
+  return (
+    <div class="page">
+      <header tabindex="0"><span className="logo-g">G</span><span className="logo-k">K</span></header>
+      <div id="nav-container">
+        <div class="bg"></div>
+        <div class="button" tabindex="0">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </div>
+        <div id="nav-content" tabindex="0">
+          <ul>
+            <li><a href="#0">Home</a></li>
+            <li><a href="#0">About</a></li>
+            <li><a href="#0">Contact</a></li>
+
+          </ul>
+        </div>
+      </div>
+      <div className="bag">
+        <i class="fas fa-suitcase"></i>
+      </div>
+    </div>
+  )
+}
+
+export default Header
